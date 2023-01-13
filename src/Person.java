1b86db036c4eebb143080b061e0b4f82c833f9b5
octavemoha <octave.moha@menloschool.org>
2023-01-13 11:34:10 -0800

    I created the person class with variables and methods

--- src/Person.java
+++ src/Person.java
@@ -9,4 +9,42 @@
 
 public class Person {
     // TODO: Complete the Person class
+    public String firstName;
+    public String lastName;
+    public String phoneNumber;
+
+    public Person(String firstName, String lastName, String phoneNumber )
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+   //toString to do
 }
+
+
+
